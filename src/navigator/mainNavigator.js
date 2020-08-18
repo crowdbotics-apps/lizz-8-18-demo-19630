18e6348ffea52b6c39e46b6ca2d58b9455b70b06
Crowdbotics <team@crowdbotics.com>
2020-08-18 18:09:47 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn2191766Navigator from '../features/SignIn2191766/navigator';
 import SignUp2291765Navigator from '../features/SignUp2291765/navigator';
 import Feed391764Navigator from '../features/Feed391764/navigator';
 
@@ -15,6 +16,7 @@ import Feed391764Navigator from '../features/Feed391764/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn2191766: { screen: SignIn2191766Navigator },
 SignUp2291765: { screen: SignUp2291765Navigator },
 Feed391764: { screen: Feed391764Navigator },
 
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn2191766Reducer from '../features/SignIn2191766/redux/reducers'
 import SignUp2291765Reducer from '../features/SignUp2291765/redux/reducers'
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn2191766: SignIn2191766Reducer,
 SignUp2291765: SignUp2291765Reducer,
 
 });
